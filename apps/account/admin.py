9cfce99b3d0eead962dfdcb8bd4ac341631ffc4d
badsha.ugv <badsha.ugv@gmail.com>
2025-08-12 18:12:50 +0600

    init

---
+++ apps/account/admin.py
@@ -0,0 +1,38 @@
+from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from .models import (
+    User,
+    Profile
+)
+
+# unregister groups
+admin.site.unregister(Group)
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'date_joined', 'is_active')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('-date_joined',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'phone_number')
+    search_fields = ('user__email', 'user__first_name', 'user__last_name')
+    ordering = ('-user__date_joined',)
