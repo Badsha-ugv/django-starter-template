8cb14155a68afb2d203c25b5ab913e4ca5074253
Hossain Badsha <67336316+Badsha-ugv@users.noreply.github.com>
2026-01-06 16:22:33 +0600

    add custom styling on password field on add user function

--- apps/account/admin.py
+++ apps/account/admin.py
@@ -31,6 +31,22 @@ class UserAdmin(BaseUserAdmin):
     )
 
 
+''''
+# unfold add user password input field style code
+def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        # This block ensures styling for password1 and password2 fields
+        if "password1" in form.base_fields:
+            form.base_fields["password1"].widget.attrs[
+                "class"
+            ] = "border border-base-200 bg-white font-medium min-w-20 placeholder-base-400 rounded-default shadow-xs text-font-default-light text-sm focus:outline-2 focus:-outline-offset-2 focus:outline-primary-600 group-[.errors]:border-red-600 focus:group-[.errors]:outline-red-600 dark:bg-base-900 dark:border-base-700 dark:text-font-default-dark dark:group-[.errors]:border-red-500 dark:focus:group-[.errors]:outline-red-500 dark:scheme-dark group-[.primary]:border-transparent px-3 py-2 w-full max-w-2xl "
+        if "password2" in form.base_fields:
+            form.base_fields["password2"].widget.attrs[
+                "class"
+            ] = "border border-base-200 bg-white font-medium min-w-20 placeholder-base-400 rounded-default shadow-xs text-font-default-light text-sm focus:outline-2 focus:-outline-offset-2 focus:outline-primary-600 group-[.errors]:border-red-600 focus:group-[.errors]:outline-red-600 dark:bg-base-900 dark:border-base-700 dark:text-font-default-dark dark:group-[.errors]:border-red-500 dark:focus:group-[.errors]:outline-red-500 dark:scheme-dark group-[.primary]:border-transparent px-3 py-2 w-full max-w-2xl "
+        return form
+'''
+
 # @admin.register(Profile)
 # class ProfileAdmin(admin.ModelAdmin):
 #     list_display = ('user', 'phone_number')
